d9c9bec2bfc05cde6baa1a9f1d2b244c82bb0877
David2261 <bulatnasirov2003@gmail.com>
2022-08-17 00:00:17 +0300

    Add filters for templates

--- bioblog/bioblog/settings.py
+++ bioblog/bioblog/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STAICFILES_DIRS = []
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
