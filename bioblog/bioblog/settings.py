c15c32619bb495a68bac9703d401136de2b48d4a
David2261 <bulatnasirov2003@gmail.com>
2022-08-20 22:51:12 +0300

    Add auth users and login and logout

--- bioblog/bioblog/settings.py
+++ bioblog/bioblog/settings.py
@@ -125,6 +125,9 @@ STAICFILES_DIRS = []
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# Ссылка при верном логине
+# LOGIN_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- bioblog/women/views.py
+++ bioblog/women/views.py
@@ -164,9 +164,6 @@ def contact(request):
 	return HttpResponse("Обратная связь")
 
 
-def login(request):
-	return HttpResponse("Авторизация")
-
 class RegisterUser(DataMixin, CreateView):
 	form_class = UserCreationForm
 	template_name = 'women/register.html'
@@ -177,6 +174,29 @@ class RegisterUser(DataMixin, CreateView):
 		c_def = self.get_user_context(title = "Регистрация")
 		return dict(list(context.items()) + list(c_def.items()))
 
+	# Авторизует пользователя сразу после регистрации
+	def form_valid(self, form):
+		user = form.save()
+		login(self.request, user)
+		return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+	form_class = LoginUserForm
+	template_name = 'women/login.html'
+
+	def get_context_data(self, *, object_list = None, **kwargs):
+		context = super().get_context_data(**kwargs)
+		c_def = self.get_user_context(title = 'Авторизация')
+		return dict(list(context.items()) + list(c_def.items()))
+
+	def get_success_url(self):
+		return reverse_lazy('home')
+
+
+def logout_user(request):
+	logout(request)
+	return redirect('login')
 
 def pageNotFound(request, exception):
 	return HttpResponseNotFound('<h1>Страница не найдена</h1>')
